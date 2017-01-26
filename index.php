40b73986b8c3567494ce94ae08eca4643a8e98d4
guledali <guled.ali01@gmail.com>
2017-01-26 13:51:26 +0100

    Added index,style and script
    Commit to master

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Glada Geten</title>
+     <link href="style.css" rel="stylesheet">
+     <script src='script.js'></script>
+  </head>
+  <body>
+    <div id="content">
+      <div id="header"></div>
+      <div>
+        <p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper accumsan lectus, et fermentum arcu suscipit sed. Mauris auctor diam eu viverra faucibus. Mauris orci turpis, fringilla in est at, fringilla aliquam diam. Curabitur in mauris vitae arcu suscipit viverra non sed risus. Proin vitae diam placerat, laoreet neque at, molestie nulla. Etiam elit elit, faucibus eget venenatis nec, viverra sed nisl. Phasellus ut bibendum lectus, in hendrerit leo. Mauris congue eros a sagittis vehicula. Quisque turpis odio, tincidunt a quam eget, volutpat commodo urna. Morbi vulputate libero mauris, sodales rhoncus metus ornare et. Cras congue sapien nec bibendum tempus. Sed arcu lacus, congue eleifend nunc eu, efficitur consequat mauris. Integer metus ipsum, elementum at vulputate id, scelerisque sit amet augue.</p>
+
+          <p> In hac habitasse platea dictumst. Phasellus tincidunt nisi non volutpat pulvinar. Vivamus gravida lorem quis ex aliquet volutpat. Curabitur pretium a quam eget laoreet. Fusce id justo velit. Vestibulum elit sem, blandit ac libero vitae, luctus suscipit arcu. Etiam in augue non nisi iaculis scelerisque. Fusce ultricies, diam nec mollis mollis, justo justo venenatis leo, sit amet accumsan felis velit ac neque. Suspendisse in dapibus libero. Ut eleifend eget lorem vel ultrices. In pulvinar quam nisl, a eleifend ante ultricies at. </p>
+
+          <p>Integer sit amet ipsum nec odio laoreet lobortis. Pellentesque euismod congue mi. Suspendisse justo turpis, fermentum vitae tincidunt nec, ullamcorper in nunc. Nullam cursus nibh ligula, non rhoncus quam ullamcorper sit amet. Donec eget posuere nisl, vel vehicula ante. Donec suscipit suscipit elementum. Fusce congue, enim et dapibus varius, odio magna pharetra nulla, sit amet hendrerit sapien augue vehicula lectus.</p>
+
+          <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla tristique massa massa, vel facilisis justo suscipit id. Suspendisse congue blandit ullamcorper. Pellentesque tincidunt ultrices pulvinar. Vivamus porttitor, purus et elementum tempus, justo enim viverra dui, pharetra tristique lectus metus id urna. Morbi et orci ac libero feugiat tempus. Integer sem odio, laoreet id velit vitae, ornare bibendum massa. Suspendisse felis mi, dictum ut urna ut, porta fermentum nulla.</p>
+      </div>
+    </div>
+
+  </body>
+</html>
